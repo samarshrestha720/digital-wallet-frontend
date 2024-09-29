73e63fa627d12e0295310808bf908c531ffe7fa8
Samar Shrestha <66566474+samarshrestha720@users.noreply.github.com>
2024-09-29 16:45:07 +0545

    Build fail fixes(TS)

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,23 +4,18 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_BASE_URL + "/api/user", // Replace with your actual API base URL
 });
 
-// Define the type for the authentication response
-type AuthResponse = {
-  token: string;
-};
-
 // Define the type for login credentials
 type LoginCredentials = {
   email: string;
   password: string;
 };
 
-// Define the type for registration data
-type RegisterData = {
-  fullname: string;
-  email: string;
-  password: string;
-};
+// // Define the type for registration data
+// type RegisterData = {
+//   fullname: string;
+//   email: string;
+//   password: string;
+// };
 
 export const login = async (credentials: LoginCredentials): Promise<String> => {
   try {
